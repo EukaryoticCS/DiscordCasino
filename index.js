01c426866b7362070b5a35a2f73a5bbeb806a6cd
DomianosPizza <damianl.official@gmail.com>
2023-02-13 09:57:33 -0700

    made outline for ping command
    Woke up bot and tried to initialize the bot as well

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello, World!")
+const dotenv = require('dotenv');
+const fs = require('node:fs');
+const path = require('node:path');
+const pingCommand = require("./commands/pingCommand")
+const { Client, GatewayIntentBits, Collection, Events,IntentsBitField, } = require("discord.js");
+dotenv.config();
+const client = new Client({intents:[IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildMessageReactions]})
+
+
+client.once(Events.ClientReady, c=>{
+    console.log('GOOOOOD MORNIN VEGAS');
+});
+
+
+client.once(Event.Client,c => {
+    pingCommand.execute();
+});
+
+
+
+client.login(process.env.TOKEN);
+
