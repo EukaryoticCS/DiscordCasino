0e3a93795f2d1980fddd584660ae7e11634de1ba
bsmithNeumont <brsmith@student.neumont.edu>
2023-02-14 15:22:47 -0700

    Made mongodb.js, connected to the Mongo database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var mongodb = require('./mongodb.js')
 const dotenv = require('dotenv');
 const fs = require('node:fs');
 const path = require('node:path');
@@ -15,6 +16,27 @@ client.once(Events.ClientReady, c=>{
 client.once(Event.Client,c => {
     pingCommand.execute();
 });
+
+
+// async function testMethodToGetAvailableFunds() {
+//     try { //Get available funds
+//         const user = await mongodb.getUser('447233441423949844');
+//         let funds = user.availableFunds;
+//         console.log(funds);
+//     } catch {
+//         // console.log("User doesn't exist!");
+//         console.log("How did you get here?")
+//     }
+
+//     try { //make a new user
+//         const newUser = await mongodb.createUser('447233441423949845');
+//         console.log(newUser);
+//     } catch {
+//         console.log("No user created...")
+//     }
+// }
+
+// testMethodToGetAvailableFunds();
 
 
 
