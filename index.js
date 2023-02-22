281d3986ec5a099b16a82c11ff17af434a960765
bsmithNeumont <brsmith@student.neumont.edu>
2023-02-22 10:02:22 -0700

    Called startBlackJack in app startup

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const fs = require('node:fs');
 const path = require('node:path');
 const pingCommand = require("./commands/pingCommand")
 const { Client, GatewayIntentBits, Collection, Events,IntentsBitField, } = require("discord.js");
+const deckofcards = require('./services/deckofcards.js');
 dotenv.config();
 const client = new Client({intents:[IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildMessageReactions]})
 
 
 client.once(Events.ClientReady, c=>{
     console.log('GOOOOOD MORNIN VEGAS');
+    deckofcards.startBlackJack();
 });
 
 
 client.once(Event.Client,c => {
-    pingCommand.execute();
+    // pingCommand.execute();
 });
 
 
