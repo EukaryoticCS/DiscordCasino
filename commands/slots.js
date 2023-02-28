311bb382c77ba2a4a5570b56469ef9f614b046d0
DomianosPizza <damianl.official@gmail.com>
2023-02-28 09:55:07 -0700

    make slots command

--- commands/slots.js
+++ commands/slots.js
@@ -1,5 +1,39 @@
-const {SlashCommandBuilder} = require("discord.js");
+const {SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require("discord.js");
 
 module.exports = {
-
+    data: new SlashCommandBuilder().setName('slots').setDescription('Brings up the slots'),
+	async execute(interaction){ 
+        const btnBlackjack = new ActionRowBuilder()
+		.addComponents(
+			new ButtonBuilder()
+			.setCustomId('btnHit')
+			.setLabel('Hit')
+			.setStyle(ButtonStyle.Primary),
+		)
+		.addComponents(
+			new ButtonBuilder()
+			.setCustomId('btnSLBet1')
+			.setLabel('Bet  1')
+			.setStyle(ButtonStyle.Success),
+		)
+        .addComponents(
+			new ButtonBuilder()
+			.setCustomId('btnSLBet10')
+			.setLabel('Bet  10')
+			.setStyle(ButtonStyle.Success),
+		)
+        .addComponents(
+			new ButtonBuilder()
+			.setCustomId('btnSLBet100')
+			.setLabel('Bet  100')
+			.setStyle(ButtonStyle.Success),
+		)
+        .addComponents(
+            new ButtonBuilder()
+            .setCustomId('btnSpin')
+            .setLabel('Spin')
+            .setStyle(ButtonStyle.Success),
+        )
+		interaction.reply({content: 'slots! :grapes: :cherries: :peach:', components: [btnBlackjack], ephemeral: true});
+	},
 }
